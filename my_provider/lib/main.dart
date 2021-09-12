a829f073e0db60c4d96866d44095d18fd415b767
yuppdev <yupp.dev@gmail.com>
2021-09-12 14:25:46 +0700

    my_provider second push
    integrate dark theme

--- my_provider/lib/main.dart
+++ my_provider/lib/main.dart
@@ -1,32 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-// import 'package:lite_rolling_switch/lite_rolling_switch.dart';
 
 void main(List<String> args) {
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => MySettings(),
-      child: const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: MyApp(),
-      ),
-    )
-
-  );
+  runApp(ChangeNotifierProvider(
+    create: (context) => MySettings(),
+    child: MyApp(),
+  ));
 }
 
 class MySettings extends ChangeNotifier {
-  String text = "Hello";
-  Color color = Colors.blue;
+  bool _isDark = false;
+  get isDark => _isDark;
+  void setBrightness(value) {
+    _isDark = value;
+    notifyListeners();
+  }
+
   bool isSwitched = false;
+
+  String text = "Hello";
   bool isChangeText = false;
+
+  Color color = Colors.blue;
   bool isChangeColor = false;
 
   void changeText() {
-    if(isChangeText == true){
+    if (isChangeText == true) {
       isChangeText = false;
       text = "Hello";
-    }else {
+    } else {
       isChangeText = true;
       text = "Text after change: \'Welcome to myApp?\'";
     }
@@ -34,20 +36,20 @@ class MySettings extends ChangeNotifier {
   }
 
   void changeColor() {
-    if(isChangeColor == true) {
+    if (isChangeColor == true) {
       isChangeColor = false;
       color = Colors.blue;
-    }else {
+    } else {
       isChangeColor = true;
       color = Colors.red;
     }
     notifyListeners();
   }
 
-  void changeSwitch(){
-    if(isSwitched == true){
+  void changeSwitch() {
+    if (isSwitched == true) {
       isSwitched = false;
-    }else{
+    } else {
       isSwitched = true;
     }
     notifyListeners();
@@ -55,218 +57,233 @@ class MySettings extends ChangeNotifier {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'App demo',
+      theme: ThemeData(
+        brightness: context.watch<MySettings>().isDark
+            ? Brightness.dark
+            : Brightness.light,
+        primaryColor: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+
+class HomeScreen extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Consumer<MySettings>(
-      builder: (context, mySettings, child) {
-        return Scaffold(
-          appBar: AppBar(
-            backgroundColor: mySettings.color,
-            title: const Text('Provider'),
-          ),
-          drawer: Drawer(
-            child: Container(
-              color: Colors.grey,
-              child: Column(
-                children: [
-                  Container(
-                    padding: const EdgeInsets.symmetric(vertical: 20),
+    return Consumer<MySettings>(builder: (context, mySettings, child) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: mySettings.color,
+          title: const Text('Provider'),
+        ),
+        drawer: Drawer(
+          child: Container(
+            color: Colors.grey,
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  decoration: const BoxDecoration(
+                    color: Colors.green,
+                    // borderRadius: BorderRadius.circular(100)
+                  ),
+                  child: const Center(
+                    child: SizedBox(
+                      height: 120,
+                      width: 120,
+                      child: CircleAvatar(
+                        backgroundImage:
+                            AssetImage('assets/images/football.jpg'),
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.only(top: 1.0),
                     decoration: const BoxDecoration(
-                      color: Colors.green,
-                      // borderRadius: BorderRadius.circular(100)
+                      gradient: LinearGradient(
+                          colors: [Colors.orange, Colors.white, Colors.white70]),
+                      color: Colors.white,
                     ),
-                    child: const Center(
-                      child: SizedBox(
-                        height: 120,
-                        width: 120,
-                        child: CircleAvatar(
-                          backgroundImage: AssetImage('assets/images/football.jpg'),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: const Icon(Icons.home),
+                            ),
+                            // const SizedBox(width: 8,),
+                            const Text(
+                              'Change Brightness',
+                              style:
+                                  TextStyle(fontSize: 18, color: Colors.black),
+                            ),
+                          ],
                         ),
-                      ),
+                        Switch(
+                          value: context.watch<MySettings>().isDark,
+                          onChanged: (value) {
+                            context.read<MySettings>().setBrightness(value);
+                          },
+                          activeTrackColor: Colors.lightGreenAccent,
+                          activeColor: Colors.green,
+                        ),
+                      ],
                     ),
                   ),
-
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child:
-                    Container(
-                      margin: const EdgeInsets.only(top: 1.0),
-                      decoration: const BoxDecoration(
-                        gradient: LinearGradient(
-                            colors: [Colors.white, Colors.white10]
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                    mySettings.changeText();
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.only(top: 1.0),
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: [Colors.orange, Colors.white, Colors.white70]),
+                      color: Colors.white,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            IconButton(
+                              onPressed: () {},
+                              icon: const Icon(Icons.arrow_forward_sharp),
+                            ),
+                            const Text(
+                              'Change text',
+                              style:
+                                  TextStyle(fontSize: 18, color: Colors.black),
+                            ),
+                          ],
                         ),
-                        color: Colors.white,
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            children: [
-                              IconButton(
-                                onPressed: () {},
-                                icon: const Icon(Icons.home),
-                              ),
-                              // const SizedBox(width: 8,),
-                              const Text(
-                                'Home',
-                                style: TextStyle(fontSize: 18, color: Colors.black),
-                              ),
-                            ],
-                          ),
-                          Switch(
-                            value: mySettings.isSwitched,
-                            onChanged: (_) {
-                              mySettings.changeSwitch();
-                            },
-                            activeTrackColor: Colors.lightGreenAccent,
-                            activeColor: Colors.green,
-                          ),
-                        ],
-                      ),
+                        Switch(
+                          value: mySettings.isChangeText,
+                          onChanged: (value) {
+                            mySettings.changeText();
+                          },
+                          activeTrackColor: Colors.lightGreenAccent,
+                          activeColor: Colors.green,
+                        ),
+                      ],
                     ),
                   ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pop(context);
-                      mySettings.changeText();
-                    },
-                    child: Container(
-                      margin: const EdgeInsets.only(top: 1.0),
-                      decoration: const BoxDecoration(
-                        gradient: LinearGradient(
-                            colors: [Colors.white, Colors.white10]
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                    mySettings.changeColor();
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.only(top: 1.0),
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: [Colors.orange, Colors.white, Colors.white70]),
+                      color: Colors.white,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            IconButton(
+                              onPressed: () {},
+                              icon: const Icon(Icons.arrow_forward_sharp),
+                            ),
+                            const Text(
+                              'Change color',
+                              style:
+                                  TextStyle(fontSize: 18, color: Colors.black),
+                            ),
+                          ],
                         ),
-                        color: Colors.white,
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            children: [
-                              const SizedBox(width: 10,),
-                              IconButton(
-                                onPressed: () {},
-                                icon: const Icon(Icons.arrow_forward_sharp),
-                              ),
-                              // const SizedBox(width: 8,),
-                              const Text(
-                                'Change text',
-                                style: TextStyle(fontSize: 18, color: Colors.black),
-                              ),
-                            ],
-                          ),
-                          Switch(
-                            value: mySettings.isChangeText,
-                            onChanged: (value) {
-                              mySettings.changeText();
-                            },
-                            activeTrackColor: Colors.lightGreenAccent,
-                            activeColor: Colors.green,
-                          ),
-                        ],
-                      ),
+                        Switch(
+                          value: mySettings.isChangeColor,
+                          onChanged: (value) {
+                            mySettings.changeColor();
+                          },
+                          activeTrackColor: Colors.lightGreenAccent,
+                          activeColor: Colors.green,
+                        ),
+                      ],
                     ),
                   ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pop(context);
-                      mySettings.changeColor();
-                    },
-                    child: Container(
-                      margin: const EdgeInsets.only(top: 1.0),
-                      decoration: const BoxDecoration(
-                        gradient: LinearGradient(
-                            colors: [Colors.white, Colors.white10]
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.only(top: 1.0),
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: [Colors.orange, Colors.white, Colors.white70]),
+                      color: Colors.white,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.logout),
                         ),
-                        color: Colors.white,
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            children: [
-                              const SizedBox(width: 10,),
-                              IconButton(
-                                onPressed: () {},
-                                icon: const Icon(Icons.arrow_forward_sharp),
-                              ),
-                              // const SizedBox(width: 8,),
-                              const Text(
-                                'Change color',
-                                style: TextStyle(fontSize: 18, color: Colors.black),
-                              ),
-                            ],
-                          ),
-                          Switch(
-                            value: mySettings.isChangeColor,
-                            onChanged: (value) {
-                              mySettings.changeColor();
-                            },
-                            activeTrackColor: Colors.lightGreenAccent,
-                            activeColor: Colors.green,
-                          ),
-                        ],
-                      ),
+                        const Text(
+                          'Log out',
+                          style: TextStyle(fontSize: 18, color: Colors.black),
+                        ),
+                      ],
                     ),
                   ),
-
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child:
-                    Container(
-                      margin: const EdgeInsets.only(top: 1.0),
-                      decoration: const BoxDecoration(
-                        gradient: LinearGradient(
-                            colors: [Colors.white, Colors.white10]
-                        ),
-                        color: Colors.white,
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          IconButton(
-                            onPressed: () {},
-                            icon: const Icon(Icons.logout),
-                          ),
-                          // const SizedBox(width: 8,),
-                          const Text(
-                            'Log out',
-                            style: TextStyle(fontSize: 18, color: Colors.black),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-
-                ],
-              ),
+                ),
+              ],
             ),
           ),
-          body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(mySettings.text),
-                  ElevatedButton(onPressed: () {mySettings.changeText();}, child: const Text('Change Text')),
-                  Switch(
-                    value: mySettings.isSwitched,
-                    onChanged: (value) {
-                        mySettings.changeSwitch();
-                    },
-                    activeTrackColor: Colors.lightGreenAccent,
-                    activeColor: Colors.green,
-                  ),
-                ],
-              )),
-        );
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(mySettings.text),
+            ElevatedButton(
+                onPressed: () {
+                  mySettings.changeText();
+                },
+                child: const Text('Change Text')),
+            Switch(
+              value: mySettings.isSwitched,
+              onChanged: (value) {
+                mySettings.changeSwitch();
+              },
+              activeTrackColor: Colors.lightGreenAccent,
+              activeColor: Colors.green,
+            ),
+          ],
+        )),
+      );
     });
-
   }
 }
